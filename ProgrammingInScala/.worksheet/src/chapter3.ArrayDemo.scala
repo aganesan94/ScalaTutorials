a6bff829345fbf3f6144a0b7576e57616377dc20
aganesan94@gmail.com <aganesan94@gmail.com>
2016-02-16 10:36:27 -0500

    Added chapter 3

---
+++ ProgrammingInScala/.worksheet/src/chapter3.ArrayDemo.scala
@@ -0,0 +1,16 @@
+package chapter3
+
+// Declaration of array
+// the "[" is replaced with "("
+// for loop has different syntax
+object ArrayDemo {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(161); 
+
+  val cars = new Array[String](3);System.out.println("""cars  : Array[String] = """ + $show(cars ));$skip(21); ;
+  cars(0) = "Honda";$skip(22); ;
+  cars(1) = "Toyota";$skip(19); ;
+  cars(2) = "BMW";$skip(23); ;
+	println(cars.length);$skip(52); ;
+  for(i <- 0 to cars.length-1){
+ 		println(cars(i))}
+  }
+}
